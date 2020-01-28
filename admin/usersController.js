444f712fb0c9864ea475aaa61f8d52abee0c222c
DanielVitorIFG <danielv759153@gmail.com>
2020-01-28 20:38:15 -0300

    Hash password

--- admin/usersController.js
+++ admin/usersController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = express('./Users.js');
+const User = require('./Users.js');
+const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req,res) => {
     res.send('Listagem de usuários');
@@ -14,7 +15,18 @@ router.post('/users/create', (req,res) => {
     let email = req.body.email;
     let password = req.body.password;
 
-    res.json({email,password});
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(password,salt);
+
+    User.create({
+        email: email,
+        password: hash
+    }).then(() => {
+        res.redirect('/');
+    }).catch(err => {
+        res.redirect('/');
+    });
+    //res.json({email,hash});
 });
 
 
